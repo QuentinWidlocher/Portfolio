40075267817d55c62e0420af4802dafd8cf66dad
Quentin Widlocher <qwidlocher@gmail.com>
2019-07-19 14:48:53 +0200

    :wrench: Add firebase config

---
+++ js/firebase/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCWiOsnhSTE-DkuvKyN3Mlbznn8xbLVrJY",
+    authDomain: "portfolio-9b0b3.firebaseapp.com",
+    databaseURL: "https://portfolio-9b0b3.firebaseio.com",
+    projectId: "portfolio-9b0b3",
+    storageBucket: "",
+    messagingSenderId: "349548091568",
+    appId: "1:349548091568:web:e9af801f4a802852"
+};
