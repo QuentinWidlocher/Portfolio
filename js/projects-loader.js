d44eb75b9e58cb20bcc4297c3a6b650edbc324bb
Quentin Widlocher <qwidlocher@gmail.com>
2019-08-14 14:27:57 +0200

    :lipstick: Edit badges appearance

--- js/projects-loader.js
+++ js/projects-loader.js
@@ -1,19 +1,23 @@
 function createCard({name, description, picture, link, tags, gradientStart, gradientEnd, gradientAngle}) {
     let badges = '';
 
+    let badgeBgColor = getMiddleColor("#" + gradientStart, "#" + gradientEnd);
+    let badgeTextColor = getTextColor(badgeBgColor);
+    let badgeStyle = `--badge-text-color: ${badgeTextColor}; --badge-bg-color: ${badgeBgColor}`;
+
     if (tags) {
         tags.forEach(tag => {
-            badges += `\n<div class="card__badge" style="background-color: #${gradientEnd}7F;">${tag}</div>`
+            badges += `\n<div class="card__badge" style="${badgeStyle}">${tag}</div>`
         });
     }
 
-    let gradient = `--gradientAngle: ${gradientAngle}deg; --gradientStart: #${gradientStart}; --gradientEnd: #${gradientEnd};`;
+    let gradientStyle = `--gradientAngle: ${gradientAngle}deg; --gradientStart: #${gradientStart}; --gradientEnd: #${gradientEnd};`;
 
     let href = (link ? `href="${link}"` : '');
 
     let htmlString = `
         <a ${href}>
-            <div class="card__gradient" style="${gradient}">
+            <div class="card__gradient" style="${gradientStyle}">
                 <img class="card_image" src="${picture}" alt="${name} Logo" width="200">
             </div>
             <div class="card__info">
@@ -33,6 +37,32 @@ function createCard({name, description, picture, link, tags, gradientStart, grad
     article.innerHTML = htmlString;
 
     return article;
+}
+
+function getMiddleColor(colorA, colorB) {
+    let c = "#";
+    for (let i = 0; i < 3; i++) {
+        let sub1 = colorA.substring(1 + 2 * i, 3 + 2 * i);
+        let sub2 = colorB.substring(1 + 2 * i, 3 + 2 * i);
+        let v1 = parseInt(sub1, 16);
+        let v2 = parseInt(sub2, 16);
+        let v = Math.floor((v1 + v2) / 2);
+        let sub = v.toString(16).toUpperCase();
+        let padsub = ('0' + sub).slice(-2);
+        c += padsub;
+    }
+    return c;
+}
+
+function getTextColor(bgColor) {
+    let m = bgColor.substr(1).match(bgColor.length == 7 ? /(\S{2})/g : /(\S{1})/g);
+    if (m) {
+        let r = parseInt(m[0], 16), g = parseInt(m[1], 16), b = parseInt(m[2], 16);
+        let brightness = ((r * 299) + (g * 587) + (b * 114)) / 1000;
+
+        return (brightness > 150 ? "black" : "white")
+    }
+    return "black";
 }
 
 
